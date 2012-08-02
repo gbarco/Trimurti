8a34bb02ef88d37b17bae665ea0bbde6e53928b8
Gonzalo Barco <gbarcouy@gmail.com>
2012-08-02 10:53:45 -0300

    Filter::CSSPacker migrated to YUI Compressor

--- Trimurti/Vishnu/FileGroup/File/HTML/Filter/CSSPacker.pm
+++ Trimurti/Vishnu/FileGroup/File/HTML/Filter/CSSPacker.pm
@@ -3,7 +3,7 @@ package Trimurti::Vishnu::FileGroup::File::HTML::Filter::CSSPacker;
 use Template::Plugin::Filter;
 use base qw( Template::Plugin::Filter );
 use Carp qw( croak );
-use CSS::Packer;
+use CSS::Compressor;
 
 sub init {
     my $self = shift;
@@ -18,55 +18,9 @@ sub filter {
     # $args = [ 'foo', 'bar' ]
     # $conf = { tag => 'script' }
 		
-		my $packer = CSS::Packer->init();
+		$text = CSS::Compressor::css_compress ( $text );
 		
-		$conf->{compress} = 'minifiy' unless defined $conf->{compress};
-
-    $packer->minify( \$text , $conf );
-
     return $text;
-
-#		$conf should be:
-#		compress
-#
-#    Defines compression level. Possible values are 'minify' and 'pretty'. Default value is 'pretty'.
-#
-#    'pretty' converts
-#
-#        a {
-#        color:          black
-#        ;}   div
-#
-#        { width:100px;
-#        }
-#
-#    to
-#
-#        a{
-#        color:black;
-#        }
-#        div{
-#        width:100px;
-#        }
-#
-#    'minify' converts the same rules to
-#
-#        a{color:black;}div{width:100px;}
-#
-#copyright
-#
-#    You can add a copyright notice at the top of the script.
-#remove_copyright
-#
-#    If there is a copyright notice in a comment it will only be removed if this option is set to a true value. Otherwise the first comment that contains the word "copyright" will be added at the top of the packed script. A copyright comment will be overwritten by a copyright notice defined with the copyright option.
-#no_compress_comment
-#
-#    If not set to a true value it is allowed to set a CSS comment that prevents the input being packed or defines a compression level.
-#
-#        /* CSS::Packer _no_compress_ */
-#        /* CSS::Packer pretty */
-#
-
 }
 
 1;
